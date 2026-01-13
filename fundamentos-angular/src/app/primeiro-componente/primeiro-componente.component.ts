ea8b32ec533e2da83e177953d58e9338846b5880
Willian Gabriel <willianlopes20@hotmail.com>
2026-01-13 09:34:14 -0300

    primeiro componente

---
+++ fundamentos-angular/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-primeiro-componente",
+    template: "<h2>Meu primeiro componente Angular</h2>",
+    styles: ["h2 { color: blue; }"]
+})
+
+export class PrimeiroComponenteComponent {}
