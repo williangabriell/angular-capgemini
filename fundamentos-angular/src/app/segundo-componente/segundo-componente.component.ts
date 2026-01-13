2b713fcd26b4fe3c3e7f15c28bd5fee015bbf1ee
Willian Gabriel <willianlopes20@hotmail.com>
2026-01-13 09:51:10 -0300

    criando components e pips via cmd

---
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+  nome = 'Willian';
+  dataNascimento = "1990-05-15";
+}
